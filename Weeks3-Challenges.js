dd46358a771298377645603e72c8133538c20a38
Meiling <happylive_2006@hotmail.com>
2023-05-07 21:54:41 -0500

    weeks3

---
+++ Weeks3-Challenges.js
@@ -0,0 +1,7 @@
+function FirstReverse(string) {
+    return string.split('').reverse().join("");
+  }
+     
+  // keep this function call here 
+  let string = "what a beautiful day!"
+  console.log(FirstReverse(string));
